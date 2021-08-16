c476278ead294e2b51f069159ccccdb1def28ab9
minardi_f@libero.it <fminardi@DESKTOP-S01D5NI.localdomain>
2021-08-16 17:21:25 +0200

    iniziato ex02

---
+++ C02/ex02/Fixed.cpp
@@ -0,0 +1,134 @@
+#include "Fixed.hpp"
+
+/* La differenza principale tra il costruttore di copie 
+e l'operatore di assegnazione è quello copy constructor 
+è un tipo di costruttore che aiuta a creare una copia di 
+un oggetto già esistente senza influenzare i valori dell'
+oggetto originale mentre l'operatore di assegnazione è 
+un operatore che aiuta ad assegnare un nuovo valore a una 
+variabile nel programma. */
+
+/* Multiply the float by 2^(number of fractional bits for the type), eg. 2^8 for 24.8
+Round the result (just add 0.5) if necessary, and floor it (or cast to an integer type) leaving an integer value.
+Assign this value into the fixed-point type. */
+
+//shifting by one bit is like dividing the number by 2, shifiting to the left is like multipling it for two 
+
+
+Fixed::Fixed()
+{
+	std::cout << "Default constructor called\n";
+}
+
+Fixed::Fixed(const int n)
+{
+	std::cout << "Int constructor called\n";
+	this->fp = n * (1<<8);
+}
+
+Fixed::Fixed(const float n)
+{
+	std::cout << "Float constructor called\n";
+	this->fp = (roundf(n*(1<<8)));
+}
+
+Fixed::Fixed (const Fixed& fixed)
+{
+	std::cout << "Copy constructor called\n";
+	*this = fixed;
+}
+
+
+int	Fixed::getRawBits(void) const
+{
+	return (this->fp);
+}
+
+void Fixed::setRawBits(int const raw)
+{
+	fp = raw;
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called\n";
+}
+
+int Fixed::toInt( void ) const
+{
+	return ((int)this->fp/(1<<8));
+}
+
+float Fixed::toFloat( void ) const
+{
+	return ((float)this->fp / (1 << this->bits));
+}
+
+std::ostream& operator<<(std::ostream& out, const Fixed& f)
+{
+   return out << f.toFloat();
+}
+
+bool Fixed::operator< (const Fixed& a, const Fixed& b)
+{
+	return b > a;
+}
+bool Fixed::operator> (const Fixed& a, const Fixed& b)
+{ 
+	return b < a; 
+}
+
+bool Fixed::operator<=(const Fixed& a, const Fixed& b)
+{
+	return !(a > b);
+}
+
+bool Fixed::operator>=(const Fixed& a, const Fixed& b)
+{ 
+	return !(a < b); 
+}
+
+bool Fixed::operator==(const Fixed& a, const Fixed& b)
+{
+	return a == b;
+}
+
+bool Fixed::operator!=(const Fixed& a, const Fixed& b)
+{ 
+	return !(a == b); 
+}
+
+Fixed& operator++()
+{
+	Fixed tmp;
+	tmp = *this;
+	operator++;
+	return (tmp);
+}
+
+Fixed operator++(int)
+{
+	operator++;
+	return *this;
+}
+
+Fixed& operator--()
+{
+	Fixed tmp;
+	tmp = *this;
+	operator--;
+	return (tmp);
+}
+
+Fixed operator--(int);
+{
+	operator--;
+	return *this;
+}
+
+Fixed& Fixed::operator = (const Fixed& assign)
+{
+	std::cout << "Assignation oerator called\n";
+	fp = assign.getRawBits();
+	return *this;
+}
---
+++ C02/ex02/Fixed.hpp
@@ -0,0 +1,44 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+#include <cmath>
+
+class Fixed
+{
+	private:
+		int fp;
+		static const int bits = 8;
+	public:
+		Fixed();
+		Fixed(const int n);
+		Fixed(const float n);
+		~Fixed();
+		Fixed (const Fixed& fixed);
+		Fixed& operator= (const Fixed& assign);
+		Fixed& operator++();
+   		Fixed operator++(int); 
+		Fixed& operator--();
+   		Fixed operator--(int);
+		bool operator< (const Fixed& lhs, const Fixed& rhs);
+		bool operator> (const Fixed& lhs, const Fixed& rhs);
+		bool operator<=(const Fixed& lhs, const Fixed& rhs);
+		bool operator>=(const Fixed& lhs, const Fixed& rhs);
+		bool operator==(const Fixed& lhs, const Fixed& rhs);
+		bool operator!=(const Fixed& lhs, const Fixed& rhs);
+		Fixed operator+(const Fixed&) const;
+		Fixed operator-(const Fixed&) const;
+		Fixed operator*(const Fixed&) const;
+		Fixed operator/(const Fixed&) const;
+		static Fixed & min(Fixed &a, Fixed &b);
+		const static Fixed & min(const Fixed &a, const Fixed &b);
+		static Fixed & max(Fixed &a, Fixed &b);
+		const static Fixed & max(const Fixed &a, const Fixed &b);
+		int getRawBits(void) const;
+		void setRawBits(int const raw);
+		float toFloat( void ) const;
+		int toInt( void ) const;
+};
+
+		std::ostream& operator<<(std::ostream& out, const Fixed& f);
+#endif
