247c3d25d5be7d88726bc793a74a36c652269dad
Francesca Minardi <fminardi@e3r8p1.42roma.it>
2021-08-28 13:34:46 +0200

    c01 finito e pushato

--- C01/ex05/Karen.cpp
+++ C01/ex05/Karen.cpp
@@ -22,22 +22,22 @@ Karen::Karen()
 
 void Karen::error(void)
 {
-	std::cout << "[DEBUG] This is unacceptable, I want to speak to the manager now." << std::endl;
+	std::cout << "[ERROR] This is unacceptable, I want to speak to the manager now." << std::endl;
 }
 
 void Karen::warning(void)
 {
-	std::cout <<"[INFO] I think I deserve to have some extra bacon for free. I’ve been coming here for years and you just started working here last month." << std::endl;
+	std::cout <<"[WARNING] I think I deserve to have some extra bacon for free. I’ve been coming here for years and you just started working here last month." << std::endl;
 }
 
 void Karen::info(void)
 {
-	std::cout << "[WARNING]I cannot believe adding extra bacon cost more money. You don’t put enough! If you did I would not have to ask for it!" << std::endl;
+	std::cout << "[INFO]I cannot believe adding extra bacon cost more money. You don’t put enough! If you did I would not have to ask for it!" << std::endl;
 }
 
 void Karen::debug(void)
 {
-	std::cout << "[ERROR] I love to get extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I just love it!" << std::endl;
+	std::cout << "[DEBUG] I love to get extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I just love it!" << std::endl;
 }
 
 
