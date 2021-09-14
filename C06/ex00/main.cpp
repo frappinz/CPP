a30fe1cae18194e1a959befa7dc54e33afe8cb5f
Francesca Minardi <fminardi@e3r11p1.42roma.it>
2021-09-14 15:47:09 +0200

    iniziato C06

---
+++ C06/ex00/main.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <iostream>
+
+int main (int argc, char *argv[])
+{
+	std::string::size_type sz;
+	if (argc == 1)
+	{
+		std::cout << "Wrong arguments\n";
+		return (0);
+	}
+	std::string num = argv[1];
+	int integer = std::stoi(num, &sz);
+}
