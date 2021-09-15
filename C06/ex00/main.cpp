a71ee8dbda9622ae074dd3afb307357b218070c3
Francesca Minardi <fminardi@e3r11p1.42roma.it>
2021-09-15 17:07:16 +0200

    che merda

--- C06/ex00/main.cpp
+++ C06/ex00/main.cpp
@@ -1,16 +1,51 @@
 #include <string>
 #include <iostream>
+//#define MAX_SIZE_T 18446744073709551615
+
+bool one_number(std::string Num)
+{
+	std::string s = Num;
+	int n = s.length();
+	char a[n + 1];
+	strcpy(a, s.c_str());
+
+	for (int i = 0; i < n; i++)
+	{
+		if (a[i] >= '0' && a[i] <= '9')
+			return (true);
+	}
+	return (false);
+}
 
 int main (int argc, char *argv[])
 {
-	std::string::size_type sz;
+	//Check arguments
 	if (argc == 1)
 	{
 		std::cout << "Wrong arguments\n";
 		return (0);
 	}
 	std::string num = argv[1];
-	int integer = std::stoi(num, &sz);
-	double _double = atof(num);
-	float _float = stof
+	bool number = one_number(num);
+	if (number)
+	{
+		size_t found = num.find("f");
+		if (found > 0 && found!=std::string::npos)
+		{
+			std::cout << "é un float!!\n";
+		}
+		found = num.find(".");
+		if (found > 0 && found!=std::string::npos)
+		{
+			std::cout << "is a double!!\n";
+		}
+		else
+			std::cout << "no\n";
+		double integer = atof(num.c_str());
+		std::cout << integer << std::endl;
+	}
+	else
+	{
+		std::cout << "only letters\n";
+	}
 }
