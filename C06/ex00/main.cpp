ec4ce8490e02e77680263a0ebab5206fa4cfbd60
Francesca Minardi <fminardi@e3r11p1.42roma.it>
2021-09-20 13:41:27 +0200

    C06 definitivo e pushato

--- C06/ex00/main.cpp
+++ C06/ex00/main.cpp
@@ -1,8 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fminardi <fminardi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/17 15:02:04 by fminardi          #+#    #+#             */
+/*   Updated: 2021/09/20 13:40:52 by fminardi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <string>
 #include <iostream>
-//#define MAX_SIZE_T 18446744073709551615
 
-bool one_number(std::string Num)
+bool is_value(float first)
+{
+	if (first != 0 && (isalpha(first) || !isdigit(first)))
+		return true;
+	return false;
+}
+
+bool is_point(std::string Num)
 {
 	std::string s = Num;
 	int n = s.length();
@@ -11,41 +29,60 @@ bool one_number(std::string Num)
 
 	for (int i = 0; i < n; i++)
 	{
-		if (a[i] >= '0' && a[i] <= '9')
+		if (a[i] == '.')
+		{
+			if (a[n - 1] == '0' || (a[n - 2] == '0' && a[n - 1] == 'f'))
+				return false;
 			return (true);
+		}
 	}
 	return (false);
 }
 
 int main (int argc, char *argv[])
 {
-	//Check arguments
+	int integer = 0;
+	float _float = 0.0f;
+	double _double = 0.0;
+	char c;
+	float first;
 	if (argc == 1)
 	{
 		std::cout << "Wrong arguments\n";
 		return (0);
 	}
 	std::string num = argv[1];
-	bool number = one_number(num);
-	if (number)
+	first = atof(num.c_str());
+	_float = static_cast<float> (first);
+	integer = static_cast<int> (first);
+	_double = static_cast<double> (first);
+	c = static_cast<char> (first);
+	bool point = is_point(num);
+	bool values = is_value(first);
+	if (first == 0 || (!point && !values)) //if value is 0 or is int and not inf and nan
 	{
-		size_t found = num.find("f");
-		if (found > 0 && found!=std::string::npos)
-		{
-			std::cout << "é un float!!\n";
-		}
-		found = num.find(".");
-		if (found > 0 && found!=std::string::npos)
-		{
-			std::cout << "is a double!!\n";
-		}
+		std::cout << "float: " << _float << ".0f" << std::endl;
+		std::cout << "int: " << integer << std::endl;
+		std::cout << "double: " << _double << ".0" << std::endl;
+		if (isprint(c))
+			std::cout << "char: '" << c << "'" << std::endl;
 		else
-			std::cout << "no\n";
-		double integer = atof(num.c_str());
-		std::cout << integer << std::endl;
+			std::cout << "char: Not displayable" << std::endl;
+		return 0;
 	}
-	else
+	if (point || values) // se é double/float o é inf/nan
 	{
-		std::cout << "only letters\n";
+		std::cout << "float: " << _float << "f" << std::endl;
+		if (values)
+			std::cout << "int: Impossible" << std::endl;
+		else
+			std::cout << "int: " << integer << std::endl;
+		std::cout << "double: " << _double << std::endl;
+		if (isprint(c))
+			std::cout << "char: '" << c << "'" << std::endl;
+		else if (values)
+			std::cout << "char: Impossible" << std::endl;
+		else
+			std::cout << "char: Not displayable" << std::endl;
 	}
 }
--- C06/ex01/main.cpp
+++ C06/ex01/main.cpp
@@ -1,18 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fminardi <fminardi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/20 13:33:55 by fminardi          #+#    #+#             */
+/*   Updated: 2021/09/20 13:33:55 by fminardi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "Data.hpp"
 
 int main()
 {
 	Data *a = new Data;
-	std::cout << "Address before serialization: " << a << std::endl;
 	uintptr_t ptr = serialize(a);
 	Data *b = new Data;
-	std::cout << "Address after serialization: " << &ptr << std::endl;
 	a = deserialize(ptr);
-	std::cout << "Address after serialization: " << a << std::endl;
 
 
 	std::cout << a << std::endl;
 	std::cout << ptr << std::endl;
 	std::cout << b << std::endl;
+
+	delete a;
+	delete b;
 
 }
